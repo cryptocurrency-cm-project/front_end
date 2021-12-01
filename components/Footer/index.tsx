94409041e34f9986a73da8adc2a913023ba85774
thomas <alstn6987@gmail.com>
2021-12-01 23:24:03 +0900

    added chart

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,22 +1,22 @@
-import React from "react";
-import styles from "../../styles/Home.module.css";
-import Image from "next/image";
+import React from 'react';
+import styles from './index.module.scss';
+import Image from 'next/image';
 
 function Footer() {
   return (
-    <footer className={styles.Footer}>
+    <footer className={styles.footer}>
       <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
+        href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
+        target='_blank'
+        rel='noopener noreferrer'
       >
-        Powered by{" "}
+        Powered by{' '}
         <span className={styles.logo}>
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          <Image src='/vercel.svg' alt='Vercel Logo' width={72} height={16} />
         </span>
       </a>
     </footer>
   );
 }
 
-export default Footer
+export default Footer;
--- pages/page1/index.tsx
+++ pages/page1/index.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React from 'react';
+import BasicTabs from '../../components/Header';
+import styles from './index.module.scss';
+import { Button, Container, Stack } from '@mui/material';
+import Pagination from '@mui/material/Pagination';
 
 function Dashboard() {
-  return <div>게시판</div>;
+  const [page, setPage] = React.useState(1);
+  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+  };
+
+  return (
+    <Container fixed>
+      <BasicTabs />
+      <Stack spacing={2} className={styles.dashboardContainer}>
+        <section className={styles.dashboard}>게시판{page}</section>
+        <Stack spacing={2}>
+          <Pagination
+            count={10}
+            shape='rounded'
+            page={page}
+            onChange={handleChange}
+          />
+        </Stack>
+      </Stack>
+    </Container>
+  );
 }
 
 export default Dashboard;
