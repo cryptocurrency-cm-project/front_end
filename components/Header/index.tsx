695a6e4cf64efbc2c80209a94945e853e3470aca
thomas <alstn6987@gmail.com>
2021-12-04 01:35:58 +0900

    Added word cloud library

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,17 +1,12 @@
 import * as React from 'react';
 import Link from 'next/link';
 import styles from './index.module.scss';
-import { NavLink } from '../NavLink';
 
 export default function BasicTabs() {
   return (
     <section className={styles.Header}>
-      <NavLink href='/' exact className='nav-item nav-link'>
-        Home
-      </NavLink>
-      <NavLink href='/page1' exact className='nav-item nav-link'>
-        Dashboard
-      </NavLink>
+      <Link href='/'>Home</Link>
+      <Link href='/page1'>Dashboard</Link>
     </section>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,15 +6,87 @@ import styles from '../styles/Home.module.scss';
 import BasicTabs from '../components/Header';
 import Footer from '../components/Footer';
 import { Button, Container, Stack } from '@mui/material';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-import { Doughnut } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import WordCloud from 'react-d3-cloud';
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+import faker from 'faker';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 const Home: NextPage = () => {
   const instance = axios.create({
     baseURL: 'https://3.18.104.128:5000',
   });
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top' as const,
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Line Chart',
+      },
+    },
+  };
+  const cloudData = [
+    { text: 'Hey', value: 1000 },
+    { text: 'lol', value: 200 },
+    { text: 'first impression', value: 800 },
+    { text: 'very cool', value: 1000000 },
+    { text: 'duck', value: 10 },
+  ];
+  const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+  ];
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data: labels.map(() =>
+          faker.datatype.number({ min: -1000, max: 1000 })
+        ),
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+      {
+        label: 'Dataset 2',
+        data: labels.map(() =>
+          faker.datatype.number({ min: -1000, max: 1000 })
+        ),
+        borderColor: 'rgb(53, 162, 235)',
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+    ],
+  };
+
   return (
     <Container fixed>
       <Head>
@@ -29,47 +101,23 @@ const Home: NextPage = () => {
       <BasicTabs />
       <Stack spacing={2}>
         <section
-          style={{ height: '100px', width: '100%', border: 'solid 1px #000' }}
+          style={{ height: 'fitContent', width: '100%', border: 'solid 1px #000' }}
         >
           암호화페 키워드
+          {typeof window === 'object' ? (
+            <WordCloud data={cloudData} />
+          ) : (
+            ''
+          )}
         </section>
         <section
           style={{ height: '100px', width: '100%', border: 'solid 1px #000' }}
         >
           해외주식 키워드
         </section>
-        <section
-          className={styles.bitCoinCart}
-        >
+        <section className={styles.bitCoinCart}>
           비트코인
-          <Doughnut
-            data={{
-              labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-              datasets: [
-                {
-                  label: '# of Votes',
-                  data: [12, 19, 3, 5, 2, 3],
-                  backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)',
-                  ],
-                  borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)',
-                  ],
-                  borderWidth: 1,
-                },
-              ],
-            }}
-          />
+          <Line options={options} data={data} />
         </section>
         <section
           style={{ height: '100px', width: '100%', border: 'solid 1px #000' }}
@@ -84,7 +132,7 @@ const Home: NextPage = () => {
         <Button
           onClick={async () => {
             try {
-              const data = await instance.get('/bit/sample');
+              const data = await instance.get('/bitgal/retrieval');
               console.log(data);
             } catch (err) {
               console.log('err:', err);
