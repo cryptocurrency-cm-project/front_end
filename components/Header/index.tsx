bc798bc6645fe65c9e564d2cfb67d20386ca9a18
thomas <alstn6987@gmail.com>
2021-12-05 20:14:32 +0900

    git flow init

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,12 +1,32 @@
-import * as React from 'react';
+import React from 'react';
 import Link from 'next/link';
 import styles from './index.module.scss';
+import { useRouter } from 'next/router';
 
 export default function BasicTabs() {
+  const router = useRouter();
+
+  console.log('Debug:router', router);
   return (
     <section className={styles.Header}>
-      <Link href='/'>Home</Link>
-      <Link href='/page1'>Dashboard</Link>
+      <Link href='/'>
+        <a
+          className={`${styles.tab} ${
+            router.pathname === '/' ? styles.on : ''
+          }`}
+        >
+          Home
+        </a>
+      </Link>
+      <Link href='/page1'>
+        <a
+          className={`${styles.tab} ${
+            router.pathname === '/page1' ? styles.on : ''
+          }`}
+        >
+          자유게시판
+        </a>
+      </Link>
     </section>
   );
 }
